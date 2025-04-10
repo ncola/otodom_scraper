2dafa82e301548c6ac1bdaa669bd4efd4a0db84d
ncola <nikola.jarzabek@edu.uekat.pl>
2025-04-10 11:09:49 +0200

    checked if it is even allowed

--- main.py
+++ main.py
@@ -8,6 +8,29 @@ from db.setup_and_manage_database import create_tables, insert_new_listing
 
 url_main = "https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/slaskie/katowice?by=LATEST&direction=DESC"
 
+import requests
+from urllib.robotparser import RobotFileParser
+
+
+def is_allowed_to_scrape(url: str) -> bool:
+    domain = '/'.join(url.split('/')[:3])
+    robots_url = domain + '/robots.txt'
+
+    try:
+        response = requests.get(robots_url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"})
+        response.raise_for_status() 
+        rp = RobotFileParser()
+        rp.parse(response.text.splitlines())
+
+        return rp.can_fetch("*", url)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Błąd podczas pobierania robots.txt: {e}")
+        return False
+
+result = is_allowed_to_scrape(url_main)
+print(f"Is fetching page {url_main} allowed?: {result}")
+
 # Utwórz tabele jezeli nie istnieją
 create_tables()
 
@@ -16,3 +39,5 @@ offers_data = scrape_all_pages()
 for offer_data in offers_data:
     insert_new_listing(offer_data)  
     print("-"*10)
+
+
