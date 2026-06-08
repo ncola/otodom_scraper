b0fb1ff0aab2869547695d357e687ea2fe905bf3
ncola <xnikola99@icloud.com>
2026-06-08 02:16:05 +0200

    fix tests

--- tests/test_otodom_scraper.py
+++ tests/test_otodom_scraper.py
@@ -1,46 +1,29 @@
-import unittest
 import logging
 import os
 import sys
 
-# ustawienie ścieżki do kodu
-scraper_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+scraper_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if scraper_path not in sys.path:
     sys.path.insert(0, scraper_path)
 
-from scraper.scraper import scrape
 from scraper.fetch_and_parse import fetch_page
 
-# konfiguracja loggera
-log_file = 'logs/test_log.txt'  
+
+os.makedirs("logs", exist_ok=True)
+
 logging.basicConfig(
-    filename=log_file,
+    filename="logs/test_log.txt",
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format="%(asctime)s - %(levelname)s - %(message)s",
 )
-logger = logging.getLogger()  
+
+logger = logging.getLogger(__name__)
 
 
-def test_fetch_page():
-    logger.info("Test: test_fetch_page")
+def test_fetch_page_returns_success_response():
     url = "https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/slaskie/katowice"
+
     response = fetch_page(url)
-    assert response.status_code == 200, f"Expected status 200 but got {response.status_code}"
-    logger.info(f"Response status code: {response.status_code} for URL: {url}")
 
-def test_scrape():
-    pass
-
-def run_tests():
-    logger.info("Rozpoczęcie testów.")
-    
-    try:
-        test_fetch_page()
-        test_scrape()
-    except AssertionError as e:
-        logger.error(f"Test failed: {e}")
-    else:
-        logger.info("Wszystkie testy zakończone pomyślnie.")
-
-if __name__ == '__main__':
-    run_tests()
+    assert response.status_code == 200
+    logger.info("Response status code: %s for URL: %s", response.status_code, url)
